e20ea17689d45ea800e6358e7be23cccad60922d
Frank Rodriguez <lokenator73@gmail.com>
2018-02-01 07:08:37 -0500

    Adding typed.js folder

---
+++ typedstuff.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $("#typed").typed({
+        strings: ['Front End Developer', '.Net Core Developer', 'a husband and father.'],
+        typeSpeed: 800,
+        backSpeed: 800,
+        backDelay: 500
+    });
+
+});
